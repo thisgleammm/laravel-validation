147129e1214684ed9b9e811704075312053f0125
thisgleammm <thisgleam@gmail.com>
2026-01-29 11:25:52 +0700

    Repopulating Forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -16,8 +16,8 @@
 
     <form action="/form" method="POST">
         @csrf
-        <label>Username : @error('username') {{ $message }} @enderror<input type="text" name="username"></label><br>
-        <label>Password : @error('password') {{ $message }} @enderror<input type="password" name="password"></label>
+        <label>Username : @error('username') {{ $message }} @enderror<input type="text" name="username" value="{{ old('username') }}"></label><br>
+        <label>Password : @error('password') {{ $message }} @enderror<input type="password" name="password" value="{{ old('password') }}"></label>
         <input type="submit" value="login">
     </form>
 </body>
